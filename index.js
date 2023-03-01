d52ecc8ca4da02224e6d05a5617094fa5ca5127b
Brian <bfeddes@hotmail.com>
2023-03-01 17:34:39 -0600

    Fetch works with server.

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
-/*
- * Getting data from the form in the index.html
- * file. Any questions let me know - Brian
-*/
-const formEl = document.querySelector('.form');
+// This function is getting the values from the html form document
+async function sendValues() {
+    const age = document.getElementById('age').value
+    const height = document.getElementById('height').value
+    const weight = document.getElementById('weight').value
+    const sysBloodPressure = document.getElementById('sysBloodPressure').value
+    const diaBloodPressure = document.getElementById('diaBloodPressure').value
+    // TODO: add cancer, alzherimers, diabetes
 
-// This will execute when someone clicks "submit"
-formEl.addEventListener('submit', event => {
-    event.preventDefault(); // prevents the page from refreshing on submission.
+    // fetching the data
+    const url = 'http://localhost:3000/calculate'
+    const queryString = `?age=${age}&height=${height}&weight=${weight}&sysBloodPressure=${sysBloodPressure}&diaBloodPressure=${diaBloodPressure}&diabetes=${diabetes}&cancer=${cancer}&alzheimers=${alzheimers}`
+    const res = await fetch(url + queryString) // fetching the string from above
+    const resText = await res.text()
+    document.getElementById('results').innerHTML = resText
 
-    const formData = new FormData(formEl); // get all data from the form.
-    const data = Object.fromEntries(formData); // storing the data as a JS object
-
-    /*
-     * fetch API will will send the data to the server (our health insurance server)
-     * current being sent to localhost:3000 for testing. Once done testing, should
-     * be sent to https://health-insurance-server.azurewebsites.net/api/data
-     * which is our server.
-    */
-    fetch('https://reqres.in/api/users', {
-        method: 'POST', // POST is for sending data to the server.
-        headers: {
-            'Content-Type': 'application/json' // Specifies what we're sending
-        }, 
-        body: JSON.stringify(data)
-    }).then(res => res.json()) // response is getting data back as JSON
-      .then(data => console.log(data)) // then it takes data and logs it
-      .catch (error => console.log(error)); // this occurs of there is an error.
-});
+}
