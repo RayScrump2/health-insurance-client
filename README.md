149c35b275aceaaac85f91e3c545122a86fe437f
Brian <bfeddes@hotmail.com>
2023-03-04 22:04:21 -0600

    Added input checking

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# Health Insurance Calculator
-NodeJS Web Server: https://health-insurance-server.azurewebsites.net
+# Health Insurance Calculator Client Side 
+Contributors: Brian Feddes, Jakob London, Jacob Prince, Jazmin Buenrostros, Bryan Moses
+<br>
+NodeJS Web Server: https://health-insurance-server.azurewebsites.net 
+<br>
 Static Web Client: https://proud-river-0c446dd10.2.azurestaticapps.net
+
 Credit to Eric Pogue for the template
--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * The above is the link to the server where we want to send the
  * data we collect from this form, which is currently on the client side.
 */
-const url = 'https://health-insurance-server.azurewebsites.net'
+const url = 'http://localhost:3000'
 // This function is getting the values from the html form document
 async function sendValues() {
     // variables
@@ -15,20 +15,34 @@ async function sendValues() {
     const alzheimers = document.getElementById('alzheimers').value
     const diabetes = document.getElementById('diabetes').value
     const heightType = document.getElementById('heightType').value 
-    let height
+    const heightInfo = document.getElementById('height')
+
+    // verifying valid input 
+    const ageInfo = document.getElementById('age')
+    ageInfo.setAttribute('min', '0')
+    const weightInfo = document.getElementById('weight')
+    weightInfo.setAttribute('min', '0')
+    const sysBloodPressureInfo = document.getElementById('sysBloodPressure')
+    sysBloodPressureInfo.setAttribute('min', '0')
+    const diaBloodPressureInfo = document.getElementById('diaBloodPressure')
+    diaBloodPressureInfo.setAttribute('min', '0')
 
     // checking if they preferred inches or feet
     if (heightType == 'inches') {
-        height = document.getElementById('height').value
+        heightInfo.setAttribute('min', '24')
+        var height = heightInfo.value
     } else if (heightType == 'feet') {
-        height = document.getElementById('height').value * 12 // converts feet back to inches for calculation purposes
+        heightInfo.setAttribute('min', '2')
+        var height = heightInfo.value * 12 // converts feet back to inches for calculation purposes
     }
-
+    const formInfo = document.getElementById('myForm')
+    if (formInfo.checkValidity()) {
     // fetching the data
     const queryString = `?age=${age}&height=${height}&weight=${weight}&sysBloodPressure=${sysBloodPressure}&diaBloodPressure=${diaBloodPressure}&diabetes=${diabetes}&cancer=${cancer}&alzheimers=${alzheimers}`
     const res = await fetch(url + "/calculate" + queryString) // fetching the string from above
     const resText = await res.text()
     document.getElementById('results').innerHTML = resText
+    }
 }
 
 async function ping() {
