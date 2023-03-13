e850bd5c2a7700a6face5166714c893a5cd1b7f1
Jacob Prince <jacobmprince@lewisu.edu>
2023-03-12 20:27:43 -0500

    Updated Height

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ async function sendValues() {
     const cancer = document.getElementById('cancer').value
     const alzheimers = document.getElementById('alzheimers').value
     const diabetes = document.getElementById('diabetes').value
-    const heightType = document.getElementById('heightType').value 
-    const heightInfo = document.getElementById('height')
+    const inches = document.getElementById('inches').value 
+    const feet = document.getElementById('feet').value
 
     // verifying valid input 
     const ageInfo = document.getElementById('age')
@@ -27,18 +27,10 @@ async function sendValues() {
     const diaBloodPressureInfo = document.getElementById('diaBloodPressure')
     diaBloodPressureInfo.setAttribute('min', '0')
 
-    // checking if they preferred inches or feet
-    if (heightType == 'inches') {
-        heightInfo.setAttribute('min', '24')
-        var height = heightInfo.value
-    } else if (heightType == 'feet') {
-        heightInfo.setAttribute('min', '2')
-        var height = heightInfo.value * 12 // converts feet back to inches for calculation purposes
-    }
     const formInfo = document.getElementById('myForm')
     if (formInfo.checkValidity()) {
     // fetching the data
-    const queryString = `?age=${age}&height=${height}&weight=${weight}&sysBloodPressure=${sysBloodPressure}&diaBloodPressure=${diaBloodPressure}&diabetes=${diabetes}&cancer=${cancer}&alzheimers=${alzheimers}`
+    const queryString = `?age=${age}&feet=${feet}&inches=${inches}&weight=${weight}&sysBloodPressure=${sysBloodPressure}&diaBloodPressure=${diaBloodPressure}&diabetes=${diabetes}&cancer=${cancer}&alzheimers=${alzheimers}`
     const res = await fetch(url + "/calculate" + queryString) // fetching the string from above
     const resText = await res.text()
     document.getElementById('results').innerHTML = resText
@@ -46,7 +38,6 @@ async function sendValues() {
     submitButton.style.display = 'none'
     document.getElementById('goAgain').innerHTML = 'Do you have another patient?'
     document.getElementById('refreshButton').removeAttribute('hidden')
-    document.getElementById('dontRefresh').removeAttribute('hidden')
     }
 }
 
